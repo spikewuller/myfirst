7e173aef38489e66c624dd9df85d98cb0db8ee1f
spikewuller <spikewuller@gmail.com>
2017-07-17 23:03:13 +0700

    Revert "add log"
    This reverts commit 3a094325c1f3ab8c7f85453c4e5d9829f39d6008.

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) {
-    console.log("ERROR: " + err);
-  }
+  if (err) throw err;
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
