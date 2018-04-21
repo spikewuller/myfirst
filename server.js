607ba0115f1ed80281382bff1b16c16bb5a5eb74
spikewuller <spikewuller@gmail.com>
2018-04-21 09:29:40 +0700

    no message

--- server.js
+++ server.js
@@ -4,31 +4,50 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var client = algoliasearch("KAGOUUA6K6", "976c775b30466d62136f2c8942416e8e");
 var http = require("https");
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+	region: "us-west-1",
+	endpoint: "https://dynamodb.us-west-1.amazonaws.com"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 var app = express();
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+	var port = server.address().port;
+	console.log("App now running on port", port);
 });
 
 // CONTACTS API ROUTES BELOW
 
 app.get("/test", function(req, res) {
-	res.status(200).send({
-		"ok":100
+	var params = {
+		TableName: "test",
+		Key:{
+			"id": req.params.id
+		}
+	};
+
+	docClient.get(params, function(err, data) {
+		if (err) {
+			console.log("Unable to read item. Error JSON:"+ JSON.stringify(err));
+		} else {
+			console.log("GetItem succeeded:"+ JSON.stringify(data));
+		}
 	});
 });
 
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
+	console.log("ERROR: " + reason);
+	res.status(code || 500).json({"error": message});
 }
