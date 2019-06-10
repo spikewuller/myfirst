b0e3b64e3cb80fc6cd3618f4faf6055b857f37ef
spikewuller <spikewuller@gmail.com>
2019-06-10 18:10:45 +0700

    no message

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.json());
 var server = http.createServer(app);
 
 app.get('/', (req, res) => {
